9720ec2c939beae7e0e94af1aa2acb63ebe6f0c6
KEVIN <you@example.com>
2022-07-06 17:05:27 +0200

    sapo mbarova detyren fundamentals 1

---
+++ C#/Fundamentals-I/Program.cs
@@ -0,0 +1,62 @@
+﻿See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+for(int i = 1; i <= 255; i++){
+    Console.WriteLine(i);
+}
+
+
+for(int i = 1; i <= 100; i++){
+    if(i % 3 == 0 || i % 5 == 0)
+    Console.WriteLine(i);
+}
+
+for(var i=1 ; i<=100 ; i++){
+    if(i % 3 == 0  && i % 5 == 0 )
+    Console.WriteLine("FizzBuzz");
+
+
+    else if(i % 3 == 0)
+    Console.WriteLine("Fizz");
+    else if(i % 5 == 0)
+    Console.WriteLine("Buzz");
+   
+    
+
+    else 
+    Console.WriteLine(i);
+}
+
+int i = 1;
+while (i < 255)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+
+
+int i =1;
+while (i <= 100){
+     if(i % 3 == 0 || i % 5 == 0)
+     Console.WriteLine(i);
+     i = i + 1;
+}
+
+int i = 1;
+while (i <= 100){
+    if(i % 3 == 0  && i % 5 == 0 )
+    Console.WriteLine("FizzBuzz");
+
+
+    else if(i % 3 == 0)
+    Console.WriteLine("Fizz");
+    else if(i % 5 == 0)
+    Console.WriteLine("Buzz");
+   
+    
+
+    else 
+    Console.WriteLine(i);
+    i = i + 1;
+}
